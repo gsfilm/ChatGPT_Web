a57d04b14ea0c1c114a0caa97c19ce6669e0774d
gsfilm <gsfilm@qq.com>
2023-04-16 22:35:16 +0800

    提交

---
+++ app/constant.ts
@@ -0,0 +1,8 @@
+export const OWNER = "GS-Media";
+export const REPO = "ChatGPT-Next-Web";
+export const REPO_URL = `http://gs-mg.cn`;
+export const ISSUE_URL = `http://gs-mg.cn`;
+export const UPDATE_URL = `http://gs-mg.cn`;
+export const FETCH_COMMIT_URL = `http://gs-mg.cn`;
+export const FETCH_TAG_URL = `http://gs-mg.cn`;
+export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
